6666ba45058856361871754b055df963777e5efa
gaurav-afk <gauravcreator123@gmail.com>
2024-08-05 17:39:09 -0400

    add and setup fragments(Home, profile)

--- app/src/main/java/com/example/a5mindev/HomePage.kt
+++ app/src/main/java/com/example/a5mindev/HomePage.kt
@@ -3,10 +3,30 @@ package com.example.a5mindev
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import androidx.fragment.app.commit
+import com.example.a5mindev.databinding.ActivityHomePageBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class HomePage : AppCompatActivity() {
+class HomePage : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    private lateinit var binding: ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        // view binding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+
+        // Attaching Fragment
+        supportFragmentManager.commit {
+            add(R.id.frame_content, FiveShortsFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
