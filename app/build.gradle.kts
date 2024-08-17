d571d2ec7b8e7478d5e966bedf62a0ffae492cfe
gaurav-afk <gauravcreator123@gmail.com>
2024-08-16 20:59:57 -0400

    Complete UI implementation for Topic page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("com.google.ai.client.generativeai:generativeai:0.1.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/a5mindev/TopicScreen.kt
+++ app/src/main/java/com/example/a5mindev/TopicScreen.kt
@@ -2,21 +2,25 @@ package com.example.a5mindev
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.a5mindev.databinding.ActivityTopicScreenBinding
 
 class TopicScreen : AppCompatActivity() {
     private lateinit var binding: ActivityTopicScreenBinding
 
-    val topicButtonClickListener = View.OnClickListener { view ->
+    private val topicButtonClickListener = View.OnClickListener { view ->
         val data = when(view.id){
-            R.id.btn_web -> "Web development"
-            R.id.btn_app -> "App development"
-            R.id.btn_game -> "Game development"
-            R.id.btn_aiml -> "AI and machine learning"
+            R.id.card_web -> "Web development"
+            R.id.card_app -> "App development"
+            R.id.card_game -> "Game development"
+            R.id.card_aiml -> "AI and machine learning"
             else -> ""
         }
         handleButtonClick(data)
@@ -26,17 +30,51 @@ class TopicScreen : AppCompatActivity() {
     private fun handleButtonClick(data: String) {
         val openSubTopicPage = Intent(this, SubTopicScreen::class.java)
         openSubTopicPage.putExtra("topic", data)
+
         startActivity(openSubTopicPage)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTopicScreenBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
 
-        binding.btnWeb.setOnClickListener(topicButtonClickListener)
-        binding.btnApp.setOnClickListener(topicButtonClickListener)
-        binding.btnGame.setOnClickListener(topicButtonClickListener)
-        binding.btnAiml.setOnClickListener(topicButtonClickListener)
+        setCardData()
+
+
+        binding.cardWeb.customTopicCard.setOnClickListener(topicButtonClickListener)
+        binding.cardApp.customTopicCard.setOnClickListener(topicButtonClickListener)
+        binding.cardGame.customTopicCard.setOnClickListener(topicButtonClickListener)
+        binding.cardAiml.customTopicCard.setOnClickListener(topicButtonClickListener)
+    }
+
+    private fun setCardData() {
+
+
+        binding.cardWeb.topic.text = "Web development"
+        binding.cardApp.topic.text = "App development"
+        binding.cardGame.topic.text = "Game development"
+        binding.cardAiml.topic.text = "AI/ML"
+
+        binding.cardWeb.topicDescription.text = "blending what we see with the hidden work that makes it all run smoothly."
+        binding.cardApp.topicDescription.text = "crafting digital dreams, where design and code blend to make your device come alive."
+        binding.cardGame.topicDescription.text = "sculpting realms of play, where creativity and code dance to spark adventure and fun."
+        binding.cardAiml.topicDescription.text = "molding the future, where data and algorithms blend to create machines that learn and think."
+
+        binding.cardWeb.customTopicCard.setCardBackgroundColor(getColor(R.color.web_color))
+        binding.cardApp.customTopicCard.setCardBackgroundColor(getColor(R.color.app_color))
+        binding.cardGame.customTopicCard.setCardBackgroundColor(getColor(R.color.game_color))
+        binding.cardAiml.customTopicCard.setCardBackgroundColor(getColor(R.color.aiml_color))
+
+        binding.cardWeb.logo.setImageResource(R.drawable.internet)
+        binding.cardApp.logo.setImageResource(R.drawable.ic_smartphone_24)
+        binding.cardGame.logo.setImageResource(R.drawable.game)
+        binding.cardAiml.logo.setImageResource(R.drawable.aiml)
+
+        binding.cardWeb.logo.setColorFilter(ContextCompat.getColor(this, R.color.green))
+        binding.cardApp.logo.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        binding.cardGame.logo.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        binding.cardAiml.logo.setColorFilter(ContextCompat.getColor(this, R.color.aiml_logo_color))
     }
 }
