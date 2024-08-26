fde702ed4a2b4d5a139a93b6658970ba64ce8e35
gaurav-afk <gauravcreator123@gmail.com>
2024-08-26 19:56:09 -0400

    config change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.a5mindev"
+    namespace = "com.towerofapp.a5mindev"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.a5mindev"
+        applicationId = "com.towerofapp.a5mindev"
         minSdk = 27
         targetSdk = 34
         versionCode = 1
